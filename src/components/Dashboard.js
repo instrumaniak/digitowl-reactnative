0d0b150e1d069ccb8e23668831d91cef72c70ca4
Raziur Rahman <instrumaniak@gmail.com>
2019-04-28 15:45:17 +0600

    UI: Dashboard: overview section with line chart.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,14 +10,31 @@ import {
   Card,
   CardItem,
   Text,
-  Body,
-  Left,
-  Right,
   Button
 } from 'native-base'
 
+
+import { LineChart, Grid, XAxis, YAxis } from 'react-native-svg-charts'
+import { Circle } from 'react-native-svg'
 import { colors } from '../config'
 import CircleButton from './CircleButton'
+
+
+const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+const Decorator = ({ x, y, data }) => {
+  return data.map(( value, index) => (
+    <Circle
+      key={index}
+      cx={ x(index) }
+      cy={ y(value)}
+      r={ 4 }
+      stroke={ colors.grey }
+      strokeWidth={3}
+      fill={colors.whiteSmoke}
+    />
+  ))
+}
 
 const Dashboard = () => (
   <View style={styles.container}>
@@ -40,27 +57,55 @@ const Dashboard = () => (
         </CardItem>
       </Card>
 
-    {/* Overview */}
-    <Card style={{ elevation: 2 }}>
-      <CardItem>
-        <Text style={styles.cardHeaderText}>Overview</Text>
-      </CardItem>
-      <CardItem>
-        <View style={styles.btnContainer}>
-          <View style={[styles.overviewButton, styles.oBtnActive]}>
-            <Text style={styles.obtnHeading}>Total Wealth</Text>
-            <Text style={styles.obtnValue}>+34 628 USD</Text>
+      {/* Overview */}
+      <Card style={{ elevation: 2 }}>
+        <CardItem>
+          <Text style={styles.cardHeaderText}>Overview</Text>
+        </CardItem>
+        <CardItem>
+          <View style={styles.btnContainer}>
+            <View style={[styles.overviewButton, styles.oBtnActive]}>
+              <Text style={styles.obtnHeading}>Total Wealth</Text>
+              <Text style={styles.obtnValue}>+34 628 USD</Text>
+            </View>
+            <View style={styles.overviewButton}>
+              <Text style={styles.obtnHeading}>Monthly Cash Flow</Text>
+              <Text style={styles.obtnValue}>+257 USD</Text>
+            </View>
           </View>
-          <View style={styles.overviewButton}>
-            <Text style={styles.obtnHeading}>Monthly Cash Flow</Text>
-            <Text style={styles.obtnValue}>+257 USD</Text>
-          </View>
-        </View>
-      </CardItem>
-    </Card>
+        </CardItem>
 
+        {/* Charts */}
+        <CardItem>
+          <View style={{ flexDirection: 'row'}}>
+            <YAxis
+              data={data}
+              style={{ marginBottom: 30, marginRight: 10 }}
+              contentInset={{ top: 10, bottom: 10 }}
+              svg={{ fill: colors.grey1 }}
+            />
+            <View style={{ flex: 1}}>
+              <LineChart
+                data={data}
+                style={{ height: 200 }}
+                svg={{stroke: colors.grey1, strokeOpacity: 0.6, strokeWidth: 3 }}
+                contentInset={{ top: 10, bottom: 10 }}
+              >
+                <Decorator />
+                <Grid svg={{strokeOpacity: 1, stroke: colors.grey}}/>
+              </LineChart>
+              <XAxis
+                style={{ marginHorizontal: -10, height: 30}}
+                data={data}
+                contentInset={{ left: 10, right: 10 }}
+                formatLabel={(value, index) => index}
+                svg={{ fill: colors.grey1 }}
+              />
+            </View>
+          </View>
+        </CardItem>
+      </Card>
     </ScrollView>
-
     <CircleButton />
   </View>
 )
@@ -104,7 +149,6 @@ const styles = StyleSheet.create({
     color: colors.grey2,
     fontSize: 25
   }
-
 })
 
 export default Dashboard
