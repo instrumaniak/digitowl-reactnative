bf849033fe974a1f6731062673b0e7a5a272a7b6
Raziur Rahman <instrumaniak@gmail.com>
2019-06-20 14:39:02 +0600

    UI: native base theme customization support added.

--- App.js
+++ App.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react'
 import { YellowBox } from 'react-native'
 import AppContainer from './src/navigation'
 
+import { StyleProvider } from 'native-base'
+import getTheme from './native-base-theme/components'
+import platform from './native-base-theme/variables/platform'
+
 // Disable some warnings
 YellowBox.ignoreWarnings([
   'Warning: Async Storage'
 ])
 
-//const navigationPersistenceKey = null
-const navigationPersistenceKey = __DEV__ ? "NavigationStateDEV" : null // eslint-disable-line
+const navigationPersistenceKey = null
+//const navigationPersistenceKey = __DEV__ ? "NavigationStateDEV" : null // eslint-disable-line
 
 // Just for dev: react-navigation state persistance is enabled
 export default class App extends Component {
   render() {
     return (
-      <AppContainer persistenceKey={navigationPersistenceKey} />
+      <StyleProvider style={getTheme(platform)}>
+        <AppContainer persistenceKey={navigationPersistenceKey} />
+      </StyleProvider>
     )
   }
 }
-
-
