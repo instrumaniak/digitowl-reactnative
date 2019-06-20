dc25aa4879be9ef73db453cd0730b5faaaa72b38
Raziur Rahman <instrumaniak@gmail.com>
2019-06-20 14:39:49 +0600

    Config: dark color scheme settings.

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
 
 import Color from 'color'
 
-export const colors = {
+const light_scheme_colors = {
   blue: '#4682B4',
 
   //green: '#00b175', spendee
@@ -32,13 +32,51 @@ export const colors = {
 
 }
 
-const brand = Color(colors.blue).desaturate(0.4).darken(0.15).hex(),
-      brandDarken = Color(brand).darken(0.05).hex(),
-      brandLighten = Color(brand).lighten(0.05).hex()
+const dark_scheme_colors = {
+  blue: '#4682B4',
+
+  //green: '#00b175', spendee
+  //greenDark: '#00a16b',
+
+  green: '#119DA4',
+
+  red: '#CC2936',
+  orange: '#F15025',
+  yellow0: '#F4D35E',
+  yellow: '#FDB833',
+
+  white: Color('#FFFFFF').darken(0.01).hex(),
+  white2: Color('#FDFDFF').darken(0.05).hex(),
+  whiteSmoke: '#F5F3F5',
+
+  //grey: '#BBC8CA', // silver sand
+  grey: '#999', // light text
+  grey1: '#ddd', // black coral // std. text color
+  grey2: '#eee', // davy's grey // header text
+  grey3: '#393D3F', // black olide
+
+  charcoal: '#383F51',
+  black: '#101419',
+}
+
+export const colors = dark_scheme_colors
+
+//'#171f46'
+// const brand = Color('#171f46').desaturate(0.25).darken(0.05).hex(),
+//       brandDarken =  Color(brand).darken(0.05).hex(),
+//       brandLighten = Color(brand).lighten(0.25).hex()
+
+const brand = Color(colors.charcoal).desaturate(0.25).darken(0.15).hex(),
+      brandDarken =  Color(brand).darken(0.15).hex(),
+      brandLighten = Color(brand).lighten(0.25).hex()
 
 export const themeColors = {
   brand,
   brandDarken,
-  brandLighten
+  brandLighten,
+
+  background: brand,
+  backgroundLighten: Color(brand).desaturate(0.25).lighten(0.15).hex(),
+  backgroundDarken: brandDarken
 }
 
